70d4abdc6479dfd3715e3172132b6678dc1d9b8c
VendaGamer <wyjeme@seznam.cz>
2024-12-17 08:38:11 +0100

    working euler

---
+++ Eulerada.cs
@@ -0,0 +1,25 @@
+﻿namespace Eulerovka;
+
+public static class Eulerada
+{
+    public static List<long> EulerovaPrvocisla(long number)
+    {
+        var factors = new List<long>();
+        
+        for (long i = 2; i * i <= number; i++)
+        {
+            while (number % i == 0)
+            {
+                factors.Add(i);
+                number /= i;
+            }
+        }
+        
+        if (number > 1)
+        {
+            factors.Add(number);
+        }
+
+        return factors;
+    }
+}
--- OknikoPravdy.cs
+++ OknikoPravdy.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics;
+
 namespace Eulerovka;
 
 public partial class OknikoPravdy : Form
@@ -5,5 +7,12 @@ public partial class OknikoPravdy : Form
     public OknikoPravdy()
     {
         InitializeComponent();
+        long a = 200;
+        var ab = Eulerada.EulerovaPrvocisla(a);
+        foreach (var ok in ab)
+        {
+            Console.WriteLine(ok);
+        }
     }
+    
 }
